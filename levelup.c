31e26e352a58a761278630bfb2d359ba879e9a0a
John Atsalakis <Johnatsa>
2024-02-27 19:37:31 +0200

    Correct logic without overflow issue

--- levelup.c
+++ levelup.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int tribonacci(int num){
-    int tri[num + 1];
-    tri[1] = 1;
-    tri[2] = 2;
-    tri[3] = 4;
+long long int tribonacci(int num){
     if(num == 1)
         return 1;
     else if(num == 2)
@@ -13,11 +9,16 @@ int tribonacci(int num){
     else if(num == 3)
         return 4;
     else{
-        for(int i = 4; i <= num; i++){
-            tri[i] = tri[i - 1] + tri[i - 2] + tri[i - 3];
+        long long int i,tn,t1 = 1, t2 = 2, t3 = 4;
+        for(i = 4; i <= num; i++){
+            tn = t1 + t2 + t3;
+            t1 = t2;
+            t2 = t3;
+            t3 = tn;    
         }
-        return tri[num];
+    return tn;
     }
+
 }
 
 
@@ -29,6 +30,6 @@ int main(int argc, char *argv[]){
     int num = atoi(argv[1]);
     //https://math.stackexchange.com/questions/3912019
     //Μας δινει πως προκειται για μια σειρα tribonacci δηλαδή ισχύει πως an=a(n−1)+a(n−2)+a(n−3)
-    printf("There are %d different ways to reach level %d\n",tribonacci(num), num);
+    printf("There are %lld different ways to reach level %d\n",tribonacci(num), num);
     return 0;
 }
